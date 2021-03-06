e2cf8bf696132dd96c040502ff6a98df5e9ca036
luizmoitinho <luizcarlos.moitinho@medlynx.com.br>
2021-03-06 20:07:56 -0300

    importando biblioteca externa

--- pacotes/go.mod
+++ pacotes/go.mod
@@ -1,3 +1,5 @@
 module modulo
 
 go 1.16
+
+require github.com/badoux/checkmail v1.2.1 // indirect
--- pacotes/main.go
+++ pacotes/main.go
@@ -3,10 +3,21 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo no arquivo main!!")
 	auxiliar.Escrever()
-	
+
+	email := "luizcarlos_costam@hotmail.com"
+	err := checkmail.ValidateFormat(email)
+
+	if err != nil {
+		fmt.Println("E-mail inválido:", email)
+		return
+	}
+	fmt.Println("E-mail valido:", email)
+
 }
