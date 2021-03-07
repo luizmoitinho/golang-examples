445dfb21e074ed78ec7641f1d534a85a942ba2c3
luizmoitinho <luizcarlos.moitinho@medlynx.com.br>
2021-03-06 21:21:11 -0300

    operadores logicos

--- operadores/operadores.go
+++ operadores/operadores.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	//Aritméticos: +, -, *, / e %
+	fmt.Printf("\nOperadores aritméticos\n")
 	soma := 10 + 2
 	subtracao := 10 - 2
 	divisao := 10 / 2
@@ -11,4 +12,14 @@ func main() {
 	restoDivisao := 10 % 2
 
 	fmt.Println(soma, subtracao, divisao, multiplicacao, restoDivisao)
+
+	fmt.Printf("\nOperadores relacionais: <, >, >=, <=, ==, !=\n")
+	fmt.Println(1 > 2)
+	fmt.Println(1 == 2)
+	fmt.Println(1 >= 2)
+	fmt.Println(1 <= 2)
+	fmt.Println(1 < 2)
+	fmt.Println(1 != 2)
+
+	fmt.Printf("\nOperadores relacionais: &&, ||, ! \n")
 }
