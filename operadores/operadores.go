8c9789063183649cc7409cfc3b1ff95093b4cac7
luizmoitinho <luizcarlos.moitinho@medlynx.com.br>
2021-03-06 21:17:14 -0300

    operadores aritmeticos

---
+++ operadores/operadores.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Aritméticos: +, -, *, / e %
+	soma := 10 + 2
+	subtracao := 10 - 2
+	divisao := 10 / 2
+	multiplicacao := 10 * 2
+	restoDivisao := 10 % 2
+
+	fmt.Println(soma, subtracao, divisao, multiplicacao, restoDivisao)
+}
