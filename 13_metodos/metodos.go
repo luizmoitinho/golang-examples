3ee26b676cd30d3df654eb0eb83a18ed7a734155
luizmoitinho <luizcarlos.moitinho@medlynx.com.br>
2021-03-10 13:15:04 -0300

    criando metodos para a struct Usuario

--- 13_metodos/metodos.go
+++ 13_metodos/metodos.go
@@ -3,16 +3,27 @@ package main
 import "fmt"
 
 type Usuario struct {
-	nome  string
-	idade uint8
+	nome   string
+	idade  uint8
+	status bool
 }
 
-func (u Usuario) salvar() {
+func (u *Usuario) salvar() {
+	u.status = true
 	fmt.Println("Salvando usuario: ", u.nome)
 }
 
+func (u *Usuario) isAdulto() bool {
+	return u.idade >= 18
+}
 func main() {
 	fmt.Println("# Métodos")
-	u1 := Usuario{"Luiz Moitinho", 22}
+
+	u1 := Usuario{"Luiz Moitinho", 22, false}
+
 	u1.salvar()
+	fmt.Println(u1)
+
+	fmt.Println("Usuario ", u1.nome, "é adulto?", u1.isAdulto())
+
 }
