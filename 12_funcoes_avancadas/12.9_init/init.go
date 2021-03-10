cb6889f1ff0166b0ce122fc27112ed9a57a4914f
luizmoitinho <luizcarlos.moitinho@medlynx.com.br>
2021-03-10 12:58:31 -0300

    inicializando uma variavel com o init

--- 12_funcoes_avancadas/12.9_init/init.go
+++ 12_funcoes_avancadas/12.9_init/init.go
@@ -2,10 +2,14 @@ package main
 
 import "fmt"
 
+var n int
+
 func init() {
 	fmt.Println("1º Funcao init")
+	n = 10
 }
 
 func main() {
 	fmt.Println("2º Funcao Main")
+	fmt.Println("Valor de n = ", n)
 }
