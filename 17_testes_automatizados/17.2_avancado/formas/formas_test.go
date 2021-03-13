bbfe2affdccbf898aea5e73476ad3ab8e1bae8d1
luizmoitinho <luizcarlos.moitinho@medlynx.com.br>
2021-03-13 00:23:34 -0300

    criando cenario de teste do retangulo

--- 17_testes_automatizados/17.2_avancado/formas/formas_test.go
+++ 17_testes_automatizados/17.2_avancado/formas/formas_test.go
@@ -5,18 +5,19 @@ import (
 	"testing"
 )
 
+type cenariosTesteRetangulo struct {
+	Retangulo
+	retornoEsperado float64
+}
+
+type cenariosTesteCirculo struct {
+	Circulo
+	retornoEsperado float64
+}
+
 func TestArea(t *testing.T) {
 
-	t.Run("Retângulo", func(t *testing.T) {
-		retangulo := Retangulo{10, 12}
-		areaEsperada := float64(120)
-		areaRecebida := retangulo.Area()
-
-		if areaEsperada != areaRecebida {
-			t.Errorf("A área recebida %f é diferente da esperada %f", areaRecebida, areaEsperada)
-		}
-
-	})
+	t.Run("Retângulo", TestRetangulo)
 
 	t.Run("Círculo", func(t *testing.T) {
 
@@ -31,3 +32,27 @@ func TestArea(t *testing.T) {
 	})
 
 }
+
+func TestRetangulo(t *testing.T) {
+
+	cenariosTeste := []cenariosTesteRetangulo{
+		{Retangulo{float64(10.0), float64(12.0)}, float64(120.0)},
+		{Retangulo{float64(10.0), float64(10.0)}, float64(0.0)},
+		{Retangulo{float64(10.0), float64(11.0)}, float64(110.0)},
+		{Retangulo{float64(16.0), float64(11.0)}, float64(176.0)},
+		{Retangulo{float64(60.0), float64(12.0)}, float64(720.0)},
+		{Retangulo{float64(8.0), float64(9.0)}, float64(72.0)},
+	}
+
+	for _, cenario := range cenariosTeste {
+		areaEsperada := cenario.retornoEsperado
+		areaRecebida := cenario.Area()
+
+		if areaEsperada != areaRecebida {
+			t.Errorf("A área recebida %f é diferente da esperada %f", areaRecebida, areaEsperada)
+		}
+	}
+
+}
+
+
---
+++ 17_testes_automatizados/17.2_avancado/go.mod
@@ -0,0 +1,3 @@
+module testes-avancados
+
+go 1.16
