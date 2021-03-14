fb845d23779ba27a9bc437f71b3654e9d0afe907
luizmoitinho <luizcarlos.moitinho@medlynx.com.br>
2021-03-14 00:37:21 -0300

    adicionando rota de usuarios

--- 19_HTTP/main.go
+++ 19_HTTP/main.go
@@ -12,6 +12,11 @@ func main() {
 		w.Write([]byte("Página Inicial"))
 	})
 
+	//URI: /usuarios
+	http.HandleFunc("/usuarios", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Carregar página de usuarios"))
+	})
+
 	log.Fatal(http.ListenAndServe(":5000", nil)) //up server
 
 }
