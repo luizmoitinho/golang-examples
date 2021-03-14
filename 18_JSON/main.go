336de1f902a91c18031a8e8af97f7f06b9717400
luizmoitinho <luizcarlos.moitinho@medlynx.com.br>
2021-03-13 23:48:27 -0300

    convertendo Map par JSON

--- 18_JSON/main.go
+++ 18_JSON/main.go
@@ -14,12 +14,25 @@ type cachorro struct {
 }
 
 func main() {
+	fmt.Printf("Struct => JSON\n\n")
 	c1 := cachorro{"Jack", "Husky", 2}
 	//Converte map ou struct para json => marshal
 	cachorroJSON, err := json.Marshal(c1)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Println(cachorroJSON)
+	fmt.Println(bytes.NewBuffer(cachorroJSON))
+
+	fmt.Printf("\n\nMap => JSON")
+	c2 := map[string]string{
+		"nome":  "Toby",
+		"raca":  "Poodle",
+		"idade": "2",
+	}
+
+	cachorroJSON, err = json.Marshal(c2)
 
 	fmt.Println(cachorroJSON)
 	fmt.Println(bytes.NewBuffer(cachorroJSON))
